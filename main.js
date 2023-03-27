8f29aa1d311078998aacfc09a6605ca36a598f2f
foguemnem <foguemjunior2018@gmail.com>
2023-03-27 22:04:08 +0100

    version_criteres_valider

--- main.js
+++ main.js
@@ -1,23 +1,10 @@
 
-//creation des variables vides qui seront stocker dans le tableau et recuperation des valeurs.
-  let infos_users = [];
-  let fullnameInput= document.getElementById('fullname');
-  let fullnameValue = fullnameInput.getAttribute('value');
-  infos_users.push(fullnameValue);
+//creation des variables vides  et recuperation des valeurs.
 
-  let usernameInput = document.getElementById('username');
-  let usernameValue = usernameInput.getAttribute('value');
-  infos_users.push(usernameValue);
-
-  let emailInput = document.getElementById('email');
-  let emailValue = emailInput.getAttribute('value');
-  infos_users.push(emailValue);
- 
-  const passwordInput = document.querySelector('#password');
-  let passwordValue = passwordInput.getAttribute('value');
-  infos_users.push(passwordValue);
-
-  //variables qui ne seront pas stocker dans le tableau
+let fullnameInput= document.getElementById('fullname');
+let usernameInput = document.getElementById('username');
+let emailInput = document.getElementById('email');
+const passwordInput = document.querySelector('#password');
 const termsCheckbox = document.querySelector('#terms');
 let submitButton = document.querySelector('#submitBtn');
 const loginInput = document.querySelector('#login');
@@ -26,50 +13,64 @@ const togglePassword = document.querySelector('#togglePassword');
 const togglePassword2 = document.querySelector('#togglePassword2');
 const togglePassword3 = document.querySelector('#togglePassword3');
 
-
-//affiche les elements saisit lorsqu'on clique sur le boutton submit
-  submitButton.addEventListener('click', function() { 
-   console.log(infos_users);
-  });
-
-
 //verification ques les criteres d'evaluations de l'email sont respectes
+const defaultEmails = ["gmail.com", "outlook.fr", "yahoo.com"];
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+var errorText3 = document.querySelector('#error-text3');
 emailInput.addEventListener('input', () => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (emailRegex.test(emailInput.value)) {
-      emailInput.setCustomValidity('');
-    } else {
-      emailInput.setCustomValidity('Veuillez entrer une adresse e-mail valide.');
-    }
-  });
+  const email = emailInput.value;
+  const domain = email.split("@")[1];
+  
+  if (defaultEmails.includes(domain) && emailRegex.test(email)) {
+    emailInput.setCustomValidity('');
+  } else {
+    emailInput.setCustomValidity('Veuillez entrer une adresse e-mail valide.');
+  }
+//validationMessage est une propriété de l’objet HTMLInputElement qui contient le message d’erreur personnalisé défini par la méthode setCustomValidity().
+// Dans ce script, la ligne de code que vous avez mentionnée met à jour le texte de l’élément HTML stocké dans la variable errorText3 avec le message d’erreur personnalisé (ou une chaîne vide si aucun message d’erreur n’est présent).
+  errorText3.textContent = emailInput.validationMessage;
+});
 
  //verification ques les criteres d'evaluations du mot de passe sont respectes 
-passwordInput.addEventListener('input', () => {
-    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
-    if (passwordRegex.test(passwordInput.value)) {
-      passwordInput.setCustomValidity('');
-    } else {
-      passwordInput.setCustomValidity('Le mot de passe doit contenir au moins 8 caractères avec des majuscules, des minuscules, des caractères spéciaux et des nombres.');
-    }
-  });
+ var errorText = document.querySelector('#error-text');
+ passwordInput.addEventListener('input', function() {
+  var password = passwordInput.value;
+   
+   if (password === password.toUpperCase()) {
+     errorText.textContent = 'Le mot de passe est entièrement en majuscule';
+   } else if (password === password.toLowerCase()) {
+     errorText.textContent = 'Le mot de passe est entièrement en minuscule';
+   } else {
+     const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+     if (passwordRegex.test(password)) {
+       errorText.textContent = '';
+     } else {
+       errorText.textContent = 'Le mot de passe doit contenir au moins 8 caractères avec des majuscules, des minuscules, des caractères spéciaux et des nombres.';
+     }
+   }
+ });
 
 //verification ques les criteres d'evaluations du nom d'utilisateur sont respectes 
+
+  var errorText2 = document.querySelector('#error-text2');
   usernameInput.addEventListener('input', () => {
     if (usernameInput.value.length >= 4) {
-        usernameInput.setCustomValidity('');
+      errorText2.textContent = '';
     } else {
-        usernameInput.setCustomValidity('Le nom doit contenir au moins 4 caractères.');
+        errorText2.textContent = 'Le nom doit contenir au moins 4 caractères.';
     }
-  });
+  })
 
 //fonction pour verifier la correspondance des deux mots de passe
   function validatePassword(){
+    var errorText4 = document.querySelector('#error-text4');
     if(password.value != confirm_password.value) {
-      confirm_password.setCustomValidity("Les mots de passe ne correspondent pas");
+      errorText4.textContent = 'Les mots de passe ne correspondent pas';
     } else {
-      confirm_password.setCustomValidity('');
+      errorText4.textContent= '';
     }
   }
+
   // Ce code ajoute des écouteurs d’événements à vos champs de mot de passe et de confirmation de mot de passe. Lorsque l’utilisateur saisit quelque chose dans l’un des champs, la fonction validatePassword est appelée pour vérifier si les deux champs contiennent la même valeur. 
   //La ligne password.onchange = validatePassword; ajoute un écouteur d’événements au champ de mot de passe qui détecte les changements dans le champ. Lorsque le champ change, la fonction validatePassword est appelée.
   //La ligne confirm_password.onkeyup = validatePassword; ajoute un écouteur d’événements au champ de confirmation de mot de passe qui détecte les frappes dans le champ. Lorsque l’utilisateur tape quelque chose dans le champ, la fonction validatePassword est appelée.
@@ -91,10 +92,10 @@ passwordInput.addEventListener('input', () => {
     if (emailInput.checkValidity() && passwordInput.checkValidity() && usernameInput.checkValidity()) {
         submitButton.disabled = false;
     } else {
-        submitButton.disabled = true;
-       
+        submitButton.disabled = true;      
     }
   }
+
   //configuartion des icones qui permettent de voire le texte saisit dans le champ des mots de passe
   togglePassword.addEventListener('click', () => {
     if (passwordInput.type === 'password') {
